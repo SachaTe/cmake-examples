2993dce4040422516499e95d3131380dc5f5977f
sacha <sacha.terzian@wrnch.ai>
2023-01-19 13:43:45 -0500

    Tmp disable

--- 08-hh-examples/hh-build-generic/build_loc.sh
+++ 08-hh-examples/hh-build-generic/build_loc.sh
@@ -7,10 +7,12 @@ DIR_SUFFIX=""
 BUILD_DIR=build$DIR_SUFFIX
 CONAN_INSTALL_DIR=conan_install$DIR_SUFFIX
 mkdir -p $BUILD_DIR $CONAN_INSTALL_DIR
+
+rm -rf $BUILD_DIR $CONAN_INSTALL_DIR
 LOCKFILE=$CONAN_INSTALL_DIR/conan.lock
-rm -rf $BUILD_DIR $CONAN_INSTALL_DIR $LOCKFILE
-conan config install ./ -sf=config --type dir
-conan lock create conanfile.py --update --build=missing --lockfile=$BASE_LOCKFILE --lockfile-out=$LOCKFILE $PROFILE_ARGS $CONAN_OPTIONS
-conan install -if $CONAN_INSTALL_DIR $PWD --build=missing --lockfile=$LOCKFILE
+
+#conan config install ./ -sf=config --type dir
+#conan lock create conanfile.py --update --build=missing --lockfile=$BASE_LOCKFILE --lockfile-out=$LOCKFILE $PROFILE_ARGS $CONAN_OPTIONS
+conan install -if $CONAN_INSTALL_DIR $PWD #--build=missing --lockfile=$LOCKFILE
 conan build -bf $BUILD_DIR -if $CONAN_INSTALL_DIR .
 #cmake --build $BUILD_DIR --config Release --target package
